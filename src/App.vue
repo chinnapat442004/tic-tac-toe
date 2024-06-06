0710b43a8d0a5f0f43ff3ee0d66ccd25e8c72c25
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-06-06 10:54:59 +0700

    Add to calculate winner function

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,66 @@
 <script setup>
 import { ref } from 'vue'
+const currentPlayer = ref('X')
+const winner = ref()
+
 const table = ref([
-  ['X', 'O', ''],
+  ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ])
+
+function checkRow() {
+  for (let row = 0; row < 3; row++) {
+    if (
+      table.value[row][0] === currentPlayer.value &&
+      table.value[row][1] === currentPlayer.value &&
+      table.value[row][2] === currentPlayer.value
+    )
+      winner.value = currentPlayer.value
+  }
+}
+
+function checkCol() {
+  for (let col = 0; col < 3; col++) {
+    if (
+      table.value[0][col] === currentPlayer.value &&
+      table.value[1][col] === currentPlayer.value &&
+      table.value[2][col] === currentPlayer.value
+    )
+      winner.value = currentPlayer.value
+  }
+}
+
+function checkX() {
+  if (
+    table.value[0][0] === currentPlayer.value &&
+    table.value[1][1] === currentPlayer.value &&
+    table.value[2][2] === currentPlayer.value
+  ) {
+    winner.value = currentPlayer.value
+  } else if (
+    table.value[0][2] === currentPlayer.value &&
+    table.value[1][1] === currentPlayer.value &&
+    table.value[2][0] === currentPlayer.value
+  ) {
+    winner.value = currentPlayer.value
+  }
+}
+
+async function getCurrentPlayer(x, y) {
+  if (table.value[x][y] === '') {
+    if (currentPlayer.value === 'X') {
+      currentPlayer.value = 'O'
+    } else {
+      currentPlayer.value = 'X'
+    }
+    table.value[x][y] = currentPlayer.value
+  }
+  //Calculate winner
+  checkRow()
+  checkCol()
+  checkX()
+}
 </script>
 
 <template>
@@ -31,12 +87,21 @@ const table = ref([
         text-align: center;
         align-items: center;
         font-size: 100px;
-        color: green;
+
         border: 3px solid white;
         background-color: #31363f;
       "
+      @click="getCurrentPlayer(x, y)"
     >
-      {{ col }}
+      <strong :class="col === 'X' ? 'text-red-lighten-1' : 'text-cyan-lighten-1'">
+        {{ col }}</strong
+      >
     </v-card>
   </v-card>
+  <div v-if="winner" style="text-align: center; font-size: 72px; margin-top: 50px">
+    winner is {{ winner }}
+  </div>
+  <div v-else style="text-align: center; font-size: 72px; margin-top: 50px">
+    Player {{ currentPlayer === 'X' ? 'O' : 'X' }}
+  </div>
 </template>
