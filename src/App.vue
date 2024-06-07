30e52b73ebf6a23c681cf2118a67526cae940279
chinnapat442004 <chinnapat.thongthai123@gmail.com>
2024-06-07 14:38:15 +0700

    Add clear Table and checkDraw function and Add WinnerDialog

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <script setup>
+import { provide } from 'vue'
 import { ref } from 'vue'
-const currentPlayer = ref('X')
-const winner = ref()
+import WinnerDialog from './WinnerDialog.vue'
 
+const currentPlayer = ref('O')
+const winner = ref()
+const dialog = ref(false)
+const empty = ref(true)
+
+provide('dialog', dialog)
+provide('winner', winner)
 const table = ref([
   ['', '', ''],
   ['', '', ''],
   ['', '', '']
 ])
+provide('table', table)
+async function clearTable() {
+  table.value = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+  winner.value = null
+  currentPlayer.value = await 'O'
+}
+async function checkDraw() {
+  if (!winner.value) {
+    outerLoop: for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        console.log(i, j, table.value[i][j])
+        if (table.value[i][j] === '') {
+          empty.value = await true
+          break outerLoop
+        } else {
+          empty.value = false
+        }
+      }
+    }
+  }
+}
 
 function checkRow() {
   for (let row = 0; row < 3; row++) {
@@ -15,8 +47,10 @@ function checkRow() {
       table.value[row][0] === currentPlayer.value &&
       table.value[row][1] === currentPlayer.value &&
       table.value[row][2] === currentPlayer.value
-    )
+    ) {
       winner.value = currentPlayer.value
+      dialog.value = true
+    }
   }
 }
 
@@ -26,8 +60,10 @@ function checkCol() {
       table.value[0][col] === currentPlayer.value &&
       table.value[1][col] === currentPlayer.value &&
       table.value[2][col] === currentPlayer.value
-    )
+    ) {
       winner.value = currentPlayer.value
+      dialog.value = true
+    }
   }
 }
 
@@ -38,12 +74,14 @@ function checkX() {
     table.value[2][2] === currentPlayer.value
   ) {
     winner.value = currentPlayer.value
+    dialog.value = true
   } else if (
     table.value[0][2] === currentPlayer.value &&
     table.value[1][1] === currentPlayer.value &&
     table.value[2][0] === currentPlayer.value
   ) {
     winner.value = currentPlayer.value
+    dialog.value = true
   }
 }
 
@@ -57,9 +95,11 @@ async function getCurrentPlayer(x, y) {
     table.value[x][y] = currentPlayer.value
   }
   //Calculate winner
+
   checkRow()
   checkCol()
   checkX()
+  checkDraw()
 }
 </script>
 
@@ -91,17 +131,41 @@ async function getCurrentPlayer(x, y) {
         border: 3px solid white;
         background-color: #31363f;
       "
-      @click="getCurrentPlayer(x, y)"
+      @click="!winner ? getCurrentPlayer(x, y) : ''"
     >
       <strong :class="col === 'X' ? 'text-red-lighten-1' : 'text-cyan-lighten-1'">
         {{ col }}</strong
       >
     </v-card>
   </v-card>
-  <div v-if="winner" style="text-align: center; font-size: 72px; margin-top: 50px">
-    winner is {{ winner }}
+
+  <div style="text-align: center; font-size: 72px; margin-top: 50px">
+    Player :
+    <strong
+      v-if="!winner"
+      :class="currentPlayer === 'O' ? 'text-red-lighten-1' : 'text-cyan-lighten-1'"
+    >
+      {{ currentPlayer === 'O' ? 'X' : 'O' }}</strong
+    >
+    <strong v-else :class="currentPlayer === 'X' ? 'text-red-lighten-1' : 'text-cyan-lighten-1'">
+      {{ currentPlayer }}</strong
+    >
   </div>
-  <div v-else style="text-align: center; font-size: 72px; margin-top: 50px">
-    Player {{ currentPlayer === 'X' ? 'O' : 'X' }}
+  <div style="text-align: center">
+    <v-btn
+      width="200"
+      height="50"
+      color="#9a8c98"
+      @click="clearTable()"
+      style="
+        color: black;
+        font-size: 30px;
+        border: 5px solid #f2e9e4;
+        border-radius: 15px;
+        margin-top: 30px;
+      "
+      >reset</v-btn
+    >
   </div>
+  <WinnerDialog></WinnerDialog>
 </template>
---
+++ src/WinnerDialog.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { inject } from 'vue'
+
+const dialog = inject('dialog')
+const winner = inject('winner')
+</script>
+<template>
+  <v-dialog v-model="dialog" width="600"
+    ><v-card height="350" color="#222831" style="border: 3px solid #e0e1dd; border-radius: 15px"
+      ><div style="text-align: center; font-size: 70px; margin-top: 50px">
+        Winner is
+        <strong :class="winner === 'X' ? 'text-red-lighten-1' : 'text-cyan-lighten-1'">
+          {{ winner }}</strong
+        >
+      </div>
+      <div style="text-align: center">
+        <v-btn
+          width="250"
+          height="60"
+          color="#9a8c98"
+          style="
+            color: black;
+            font-size: 30px;
+            border: 5px solid #f2e9e4;
+            border-radius: 15px;
+            margin-top: 75px;
+          "
+        >
+          Regeme</v-btn
+        >
+      </div>
+    </v-card></v-dialog
+  >
+</template>
